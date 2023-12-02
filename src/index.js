56ce6dac65401d9958e342b1f55fd98fd8d2fa30
inderjit-fullstack-dev <inderjit.fullstack.dev@gmail.com>
2023-12-02 17:40:30 +0530

    generating questions using ai

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import * as pdfjs from "pdfjs-dist/build/pdf";
+import "pdfjs-dist/build/pdf.worker";
 import App from "./App";
 import "./index.css";
-
+pdfjs.GlobalWorkerOptions.workerSrc = "path/to/pdf.worker.js";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
--- src/pages/job-experience/experience.js
+++ src/pages/job-experience/experience.js
@@ -1,6 +1,166 @@
-import React from "react";
-import "./experience.css";
+import React, { useRef, useState } from "react";
+import { Dropdown } from "primereact/dropdown";
+import { Button } from "primereact/button";
+import { Toast } from "primereact/toast";
+import * as pdfjs from "pdfjs-dist";
 
+import "./experience.css";
+import getAIResult from "../../utils/gptUtility";
+import { TailSpin } from "react-loader-spinner";
 export default function Experience() {
-  return <div>experience</div>;
+  const [experience, setExperience] = useState(null);
+  const [uploadedFile, setUploadedFile] = useState(null);
+  const [loader, setLoader] = useState(false);
+  let [pageText, setPageText] = useState("");
+  const toast = useRef(null);
+  const data = [
+    { name: "You are a newbie", code: "no experience" },
+    { name: "Under 5 Years", code: "less than 5 years of experience" },
+    { name: "Above 5 Years", code: " more than 5 years of experience" },
+  ];
+
+  const handleFileChange = (event) => {
+    const selectedFile =
+      event.target.files && event.target.files.length > 0
+        ? event.target.files[0]
+        : null;
+
+    if (selectedFile && selectedFile.size <= 5 * 1024 * 1024) {
+      setUploadedFile(selectedFile);
+
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        const pdfData = new Uint8Array(e.target.result);
+        loadAndParsePDF(pdfData);
+      };
+
+      reader.readAsArrayBuffer(selectedFile);
+    } else {
+      alert("File size exceeds the limit (5MB) or no file selected.");
+    }
+  };
+
+  const parseQuestions = (text) => {
+    const questions = text.split("\n");
+    const questionObj = questions.map((question, index) => {
+      return {
+        index: index,
+        question: question,
+        answer: "",
+      };
+    });
+
+    localStorage.setItem("questions", JSON.stringify(questionObj));
+  };
+
+  const handleProceedNext = async () => {
+    if (!experience) {
+      toast.current.show({
+        severity: "error",
+        summary: "Error",
+        detail: "You need to select experience!",
+        life: 2000,
+      });
+      return;
+    }
+
+    if (!pageText) {
+      toast.current.show({
+        severity: "error",
+        summary: "Error",
+        detail: "No Keyword found in your resume.",
+        life: 2000,
+      });
+      return;
+    }
+    try {
+      setLoader(true);
+      const prompt = `Generate 12 technical and coding-related only interview questions with no answer for a candidate having ${experience}, focusing on their expertise in ${pageText}.`;
+      const result = await getAIResult(prompt);
+      setLoader(false);
+
+      if (result) {
+        parseQuestions(result);
+      }
+    } catch (error) {
+      setLoader(false);
+      console.log("error gpt", error);
+    }
+  };
+
+  const loadAndParsePDF = async (pdfData) => {
+    try {
+      const pdf = await pdfjs.getDocument({ data: pdfData }).promise;
+      const numPages = pdf.numPages;
+      let pageText = "";
+      for (let pageNum = 1; pageNum <= numPages; pageNum++) {
+        const page = await pdf.getPage(pageNum);
+        const textContent = await page.getTextContent();
+        const text = textContent.items.map((item) => item.str).join(" ");
+
+        pageText += " " + text;
+        setPageText(pageText);
+      }
+    } catch (error) {
+      console.error("Error loading PDF:", error);
+    }
+  };
+
+  return (
+    <>
+      {loader && (
+        <TailSpin
+          height="80"
+          width="80"
+          color="#fff"
+          ariaLabel="tail-spin-loading"
+          radius="1"
+          wrapperStyle={{
+            backgroundColor: "rgba(0, 0, 0, .8)",
+            height: "100vh",
+            position: "absolute",
+            width: "100vw",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            margin: 0,
+            padding: 0,
+            zIndex: 100,
+          }}
+          wrapperClass=""
+          visible={true}
+        />
+      )}
+      <Toast ref={toast} />
+      <Dropdown
+        value={experience}
+        onChange={(e) => setExperience(e.value)}
+        options={data}
+        optionLabel="name"
+        placeholder="Your Experience..."
+        className="w-full md:w-14rem"
+      />
+      <div>
+        <h3>Resume (PDF - Max 5MB)</h3>
+        <input
+          type="file"
+          accept="application/pdf"
+          onChange={handleFileChange}
+        />
+        {uploadedFile && (
+          <div>
+            <h3>Uploaded File</h3>
+            <p>{uploadedFile.name}</p>
+          </div>
+        )}
+      </div>
+
+      <Button
+        // disabled={!uploadedFile}
+        label="Proceed Next"
+        onClick={handleProceedNext}
+      />
+    </>
+  );
 }
